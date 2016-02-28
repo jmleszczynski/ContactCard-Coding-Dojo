ea50847a51176784dc0e4d950ab346ed07f1552e
jmleszczynski <jmleszczynski@gmail.com>
2016-02-28 16:35:24 -0500

    first commit

---
+++ ContactCard.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+		$("button").click(function(){
+			$("#cards").append("$(".first").val() + $(".last").val() + <br> + $(".description").val()")
+				$("#enter").val("");
+				return false;
+		});
+});
